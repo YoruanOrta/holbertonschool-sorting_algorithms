ea836af683313dffa5c2be61413c2538a2192acf
Yoruan <yoruanorta50@gmail.com>
2024-12-14 12:42:53 -0400

    Making the task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,27 +10,24 @@
 
 void bubble_sort(int *array, size_t size)
 {
+	int temp;
+	int swapped;
+
 	for (size_t i = 0; i < size - 1; i++)
 	{
-		for (size_t j = 0; j < size - i - 1; j++)
+		swapped = 0;
+		for (size_t j = 0; j < size - 1 - i; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				int temp = array[j];
-
+				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-
-				for (size_t k = 0; k < size; k++)
-				{
-					printf("%d", array[k]);
-					if (k < size - 1)
-					{
-						printf(" ");
-					}
-				}
-				printf("\n");
+				swapped = 1;
+				print_array(array, size);
 			}
 		}
+		if (!swapped)
+		break;
 	}
 }
